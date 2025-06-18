a6ed44a83e80a4f846c6f3ba40c106cbb396bdaa
jellyseop <jellyjun7777@naver.com>
2025-06-18 15:27:42 +0900

    initial commit

---
+++ src/main/java/com/welab/k8s_backend_alim/advice/ApiCommonAdvice.java
@@ -0,0 +1,44 @@
+package com.welab.k8s_backend_alim.advice;
+
+
+
+import com.welab.k8s_backend_alim.common.dto.ApiResponseDto;
+import com.welab.k8s_backend_alim.common.exception.BadParameter;
+import com.welab.k8s_backend_alim.common.exception.ClientError;
+import com.welab.k8s_backend_alim.common.exception.NotFound;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@Order(1)
+@RestControllerAdvice
+public class ApiCommonAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFound.class})
+    public ApiResponseDto<String> handleNotFound(NotFound e) {
+        return ApiResponseDto.createError(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({BadParameter.class})
+    public ApiResponseDto<String> handleBadParameter(BadParameter e) {
+        return ApiResponseDto.createError(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ClientError.class})
+    public ApiResponseDto<String> handleClientError(ClientError e) {
+        return ApiResponseDto.createError(e.getErrorCode(), e.getErrorMessage());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({Exception.class})
+    public ApiResponseDto<String> handleException(Exception e) {
+        log.error(e.getMessage());
+        return ApiResponseDto.createError("Server Error", e.getMessage());
+    }
+}
